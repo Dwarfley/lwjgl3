9253c3b370fbdb79b9a419060d8337d81e1438e9
Niklas Johansson <dwarfley.private@gmail.com>
2025-07-22 16:27:54 +0200

    build: include an empty Gradle project for each release module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,19 @@
 rootProject.name = "Root"
 
 includeBuild("build-logic")
+
+val modulesDir = file("bin/RELEASE/")
+
+if (modulesDir.exists() && modulesDir.isDirectory) {
+    modulesDir.listFiles { file -> file.isDirectory }.forEach { dir ->
+        val moduleId = dir.name
+        val moduleDir = file("bin/GRADLE/${moduleId}")
+
+        if (!moduleDir.exists()) {
+            moduleDir.mkdirs();
+        }
+
+        include(":${moduleId}")
+        project(":${moduleId}").projectDir = moduleDir
+    }
+}
