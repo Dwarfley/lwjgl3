6ae2252298fb47cf6b5ab09e40968e4de2019d09
Niklas Johansson <dwarfley.private@gmail.com>
2025-08-03 18:04:22 +0200

    build: formatted modules to new syntax

---
+++ publish/lwjgl-modules/lwjgl-vulkan/build.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * Copyright LWJGL. All rights reserved.
+ * License terms: https://www.lwjgl.org/license
+ */
+plugins {
+    id("lwjgl-module")
+}
+
+lwjglPlatforms {
+    addSupport(MACOS, NATIVE_REQUIRED)
+}
+
+dependencies {
+    implementation(project(":lwjgl-modules:lwjgl"))
+}
+
+lwjglPublication.create {
+    title("LWJGL - Vulkan bindings")
+    description("A new generation graphics and compute API that provides high-efficiency, cross-platform access to modern GPUs used in a wide variety of devices from PCs and consoles to mobile phones and embedded platforms.")
+}
