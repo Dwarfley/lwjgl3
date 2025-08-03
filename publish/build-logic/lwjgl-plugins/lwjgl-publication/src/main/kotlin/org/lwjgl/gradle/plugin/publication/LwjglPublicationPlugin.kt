42f29449d484fe169c9bd7a0f462beaf0f84e2b4
Niklas Johansson <dwarfley.private@gmail.com>
2025-08-03 20:18:39 +0200

    build: create publication plugin

--- publish/build-logic/lwjgl-plugins/lwjgl-publication/src/main/kotlin/org/lwjgl/gradle/plugin/publication/LwjglPublicationPlugin.kt
+++ publish/build-logic/lwjgl-plugins/lwjgl-publication/src/main/kotlin/org/lwjgl/gradle/plugin/publication/LwjglPublicationPlugin.kt
@@ -5,9 +5,64 @@
 package org.lwjgl.gradle.plugin.publication
 
 import org.gradle.api.*
+import org.gradle.api.publish.*
+import org.gradle.api.publish.maven.*
+import org.gradle.api.publish.maven.plugins.*
+import org.gradle.kotlin.dsl.*
+import org.lwjgl.gradle.utils.*
+
+class LwjglPublication internal constructor() {
+    internal var title: String = ""
+    internal var description: String = ""
+
+    fun title(title: String) {
+        this.title = title
+    }
+
+    fun description(description: String) {
+        this.description = description
+    }
+}
+
+open class LwjglPublicationExtension(
+    private val project: Project
+) {
+    private val actions = mutableListOf<(MavenPublication) -> Unit>()
+
+    fun create(action: Action<LwjglPublication>) {
+        val publication = LwjglPublication()
+        action.execute(publication)
+        project.extensions.configure<PublishingExtension> {
+            publications {
+                create<MavenPublication>(project.name.toPascalCase()) {
+                    artifactId = project.name
+
+                    pom {
+                        name.set(publication.title)
+                        description.set(publication.description)
+                    }
+
+                    actions.forEach { action ->
+                        action.invoke(this)
+                    }
+                }
+            }
+        }
+    }
+
+    fun all(action: (MavenPublication) -> Unit) {
+        actions.add(action)
+    }
+}
 
 class LwjglPublicationPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        
+        project.pluginManager.apply(MavenPublishPlugin::class.java)
+        project.extensions.create(
+            LwjglPublicationExtension::class.java,
+            "lwjglPublication",
+            LwjglPublicationExtension::class.java,
+            project
+        )
     }
 }
